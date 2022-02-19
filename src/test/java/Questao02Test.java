304ee872a7ba3d28f11afc4e648a0f9e3b3a6eb2
marcelofelipesilva <marcelofelipesilva@outlook.com>
2022-02-19 16:32:50 -0300

    atualizado Questao02Test.java

--- src/test/java/Questao02Test.java
+++ src/test/java/Questao02Test.java
@@ -6,7 +6,7 @@ class Questao02Test {
     @Test
     void testeTamanhoSenha() {
         String senha = "Ya3";
-        int resultadoEsperado = 6 -senha.length();
+        int resultadoEsperado = 6 - senha.length();
         int resultadoAtual = Questao02.testeTamanhoSenha("Ya3");
 
         Assertions.assertEquals(resultadoEsperado,resultadoAtual);
